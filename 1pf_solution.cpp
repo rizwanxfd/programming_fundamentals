f712a1fb96b0e81dc25e59f8b554b80d7c57d83d
Muhammad Rizwan Tahir <therizwantahir@gmail.com>
2025-08-11 23:18:14 +0500

    Add files via upload

---
+++ 1pf_solution.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+using namespace std;
+
+// Q10
+int main()
+{
+    int r;
+    float a;
+    cout << "Enter radius of a circle: ";
+    cin >> r;
+    a = 3.14f * r * r;
+    cout << "Area is " << fixed << setprecision(2) << a << endl;
+    return 0;
+}
+
+/*
+// Q9
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l, b, a;
+    cout << "Enter length and breadth of a rectangle: ";
+    cin >> l >> b;
+    a = l * b;
+    cout << "Area is " << a << endl;
+    return 0;
+}
+*/
+
+/*
+// Q8
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter a number: ";
+    cin >> a;
+    b = a * a;
+    cout << "Square of " << a << " is " << b << endl;
+    return 0;
+}
+*/
+
+/*
+// Q7
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "Sum of " << a << " and " << b << " is " << c << endl;
+    return 0;
+}
+*/
+
+/*
+// Q6
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\"Teacher's Day\"" << endl;
+    return 0;
+}
+*/
+
+/*
+// Q5
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\\r" << endl;
+    return 0;
+}
+*/
+
+/*
+// Q4
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\\n" << endl;
+    return 0;
+}
+*/
+
+/*
+// Q3
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\"MySirG\"" << endl;
+    return 0;
+}
+*/
+
+/*
+// Q2
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello\nStudents" << endl;
+    return 0;
+}
+*/
+
+/*
+// Q1
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello" << endl;
+    return 0;
+}
+*/
+
